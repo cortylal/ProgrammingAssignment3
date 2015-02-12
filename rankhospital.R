bc35cdbf167bc883818447a40abc766468e6d603
cortylal <alexandre.cortyl@etu.utc.fr>
2015-02-12 16:08:36 +0100

    addition of rankhospital.R

---
+++ rankhospital.R
@@ -0,0 +1,10 @@
+## This function's goal is to return the name of the Hospital that has the 
+## ranking "num", considering the morality rate for a specified condition 
+## in a specific US state
+
+rankhospital <- function(state, outcome, num = "best") {
+        ## Read outcome data
+        ## Check that state and outcome are valid
+        ## Return hospital name in that state with the given rank
+        ## 30-day death rate
+}
