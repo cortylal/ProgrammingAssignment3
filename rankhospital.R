8398071889d8066cb5ec8e1b151c534650c05b6c
cortylal <alexandre.cortyl@etu.utc.fr>
2015-02-12 17:44:20 +0100

    Final version (Submitted) Works! :)

--- rankhospital.R
+++ rankhospital.R
@@ -1,5 +1,5 @@
 ## This function's goal is to return the name of the Hospital that has the 
-## ranking "num", considering the morality rate for a specified condition 
+## ranking "num", considering the mortality rate for a specified condition 
 ## in a specific US state
 
 rankhospital <- function(state, outcome, num = "best") {
@@ -7,12 +7,11 @@ rankhospital <- function(state, outcome, num = "best") {
         complete_outcome <- read.csv("outcome-of-care-measures.csv", na.strings = "Not Available")
         
         ## Check that state and outcome are valid
-        ## Is the state in the data frame?
-        if (!(state %in% complete_outcome$State)) {     
+        ##
+        if (!(state %in% complete_outcome$State)) {                     ## Is the state in the data frame?    
                 stop("invalid state")
         }
-        ## Is the outcome valid?
-        if(outcome != "heart attack" & outcome != "heart failure" 
+        if(outcome != "heart attack" & outcome != "heart failure"       ## Is the outcome valid?
            & outcome != "pneumonia") {
                 stop("invalid outcome")
         }
@@ -30,25 +29,22 @@ rankhospital <- function(state, outcome, num = "best") {
         wanted_data <- state_outcome[,c("Hospital.Name", outcome_column_name)]
         ## Remove the NAs in outcome_column_name
         wanted_data <- wanted_data[!is.na(as.numeric(as.character(wanted_data[,outcome_column_name]))),]
+        ## Sorting the whole by death rate first, by hospital name if there's a tie 
+        ordered_data <- wanted_data[order(wanted_data[,outcome_column_name],wanted_data[,"Hospital.Name"]),]
         
-        
-        
-        ## Setting the requested rank as numeric
-        if (num == "best") {
+        ## Setting the rank as numeric
+        ##
+        if (num == "best") {    ## Return the first name
                 num <- 1
         }
-        if (num == "worst") {
-                num <- nrow(wanted_data)
-                
-                ##last <- as.vector(tail(n=1,complete_outcome[,"Hospital.Name"]))
-        }
-        if (num > nrow(wanted_data)) {
-                return(NA)
+        if (num == "worst") {   ## Return the last name 
+                num <- nrow(ordered_data)
         }
         
-        
-        ## Return hospital name in that state with the given rank
-        ## 30-day death rate
+        ## Return hospital name in the given rank
+        ##
+        hospital_name <- as.vector(ordered_data[num,"Hospital.Name"])
+        return(hospital_name)
 }
 
 ## This function is used to toupper every first letter of a sentence
