0c70ab2bd70da6f11148e7cc066f25a6cd0cb12f
cortylal <alexandre.cortyl@etu.utc.fr>
2015-02-18 15:16:16 +0100

    cleaner loop on USA states

--- nationalnumber.R
+++ nationalnumber.R
@@ -10,14 +10,21 @@ nationalnumber <- function(outcome) {
                 stop("invalid outcome")
         }
         
-        ## Read outcome data, convert "Not Available" into proper NAs for removal later
-        complete_outcome <- read.csv("outcome-of-care-measures.csv", na.strings = "Not Available")
+        ## Read outcome data, but only keep the State column
+        complete_outcome <- read.csv("outcome-of-care-measures.csv", 
+                                     colClasses = c(rep("NULL",6), "character", rep("NULL", 39)), 
+                                     header = TRUE)
+        ## Create the vector of different states
+        state <- character(0)
+        for (s_name in unique(complete_outcome$State)) {
+                state <- c(state, s_name)
+        }
         
         ## Create the total number to return
         national_number <- 0
         
         ## For each state, get the number of patient for the outcome and add to national_number
-        for (s_name in unique(complete_outcome$State)) {
+        for (s_name in state) {
                 national_number <- national_number + patientnumber(s_name, outcome)
         }
         
