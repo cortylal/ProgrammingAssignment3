b73146559eed4754d678cd8ff4aaaad4463de617
cortylal <alexandre.cortyl@etu.utc.fr>
2015-02-18 14:39:30 +0100

    nationalnumber.R added

---
+++ nationalnumber.R
@@ -0,0 +1,26 @@
+## This function's goal is to return the number of patient for a given oucome
+## in the entire USA. Uses patientnumber() from numberpatient.R
+
+nationalnumber <- function(outcome) {
+        ## Check that the outcome is valid
+        ##
+        ## Is the outcome valid?
+        if(outcome != "heart attack" & outcome != "heart failure" 
+           & outcome != "pneumonia") {
+                stop("invalid outcome")
+        }
+        
+        ## Read outcome data, convert "Not Available" into proper NAs for removal later
+        complete_outcome <- read.csv("outcome-of-care-measures.csv", na.strings = "Not Available")
+        
+        ## Create the total number to return
+        national_number <- 0
+        
+        ## For each state, get the number of patient for the outcome and add to national_number
+        for (s_name in unique(complete_outcome$State)) {
+                national_number <- national_number + patientnumber(s_name, outcome)
+        }
+        
+        ## Return the total number of patient treated for the outcome in the USA
+        return(national_number)
+}
